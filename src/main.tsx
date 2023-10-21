1efce4187258e3f31a01c1c1743d4b86851df116
Solo <juan@xmartlabs.com>
2023-10-21 02:17:46 -0300

    renaming on root app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import TranslatorApp from './TranslatorApp.tsx'
 import './index.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TranslatorApp />
   </React.StrictMode>
 )
